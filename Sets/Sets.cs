ea1639d2e79362addf07242223cea85c9176fd0b
GitOtter <p@q.com>
2016-02-10 18:58:44 +0100

    Initial commit.

---
+++ Sets/Sets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sets
+{
+	public class Sets
+	{
+		public static List<int> GetUnion( List<int> setA, List<int> setB )
+		{
+			return new List<int>();
+		}
+
+		public static List<int> GetIntersection(List<int> setA, List<int> setB)
+		{
+			return new List<int>();
+		}
+
+		public static List<int> GetDifference(List<int> setA, List<int> setB)
+		{
+			return new List<int>();
+		}
+
+		public static bool HasMember(List<int> setA, int elem)
+		{
+			return true;
+		}
+
+		public static bool IsSubsetOf(List<int> setA, List<int> setB)
+		{
+			return true;
+		}
+
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
